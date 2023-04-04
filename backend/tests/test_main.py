6e7d9aad6070ed59e034deb2838de4c878b61ea3
joshling1919 <josh@podsie.org>
2023-04-04 13:47:56 -0700

    Write specs

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from unittest.mock import patch, Mock
 from app.main import app
 
 client = TestClient(app)
@@ -11,20 +12,25 @@ def test_root_endpoint():
 
 
 def test_generate_concept_map_success():
-    response = client.post("/generate_concept_map", params={"topic": "Python"})
+    with patch("openai.Completion.create") as mock_completion:
+        mock_completion.return_value.choices[
+            0
+        ].text.strip.return_value = "mocked concept map"
+        response = client.post("/generate_concept_map",
+                               json={"prompt": "Python"})
     assert response.status_code == 200
-    # Add more assertions to check the response content as needed
+    assert response.json() == {"concept_map": "mocked concept map"}
 
 
 def test_generate_concept_map_missing_prompt():
     response = client.post("/generate_concept_map")
     assert response.status_code == 422
     assert response.json() == {
-        "detail": [
+        'detail': [
             {
-                "loc": ["query", "topic"],
-                "msg": "field required",
-                "type": "value_error.missing",
+                'loc': ['body'],
+                'msg': 'field required',
+                'type': 'value_error.missing'
             }
         ]
     }
